1fe1285b91f686d9c911c1bc6b491ed8d387161b
William Holcombe <william.holcombe@uconn.edu>
2018-07-29 12:17:16 -0400

    set up router

---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const PhotoGrid = React.createClass({
+    render() {
+        return (
+            <div className="single-photo">
+I'm a PhotoGrid
+            </div>
+        )
+    }
+});
+export default PhotoGrid;
